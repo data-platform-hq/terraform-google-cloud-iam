0963ce8eca85444c0f426338399040d454af0e05
Ruslan Riznyk <ruslan_riznyk@epam.com>
2023-08-04 11:41:11 +0300

    feat: updated module 	modified:   versions.tf

--- versions.tf
+++ versions.tf
@@ -3,11 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.24.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">=3.4.3"
+      version = ">= 4.69.1"
     }
   }
 }
