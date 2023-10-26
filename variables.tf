a7856db781f5a53152faf4a71f1742851cf657ab
Ruslan Riznyk <ruslan_riznyk@epam.com>
2023-10-26 11:37:07 +0300

    chore: updated 'custom role' 	modified:   README.md 	modified:   main.tf 	modified:   variables.tf 	modified:   versions.tf

--- variables.tf
+++ variables.tf
@@ -19,20 +19,25 @@ variable "iam_entity" {
   description = <<-EOT
   IAM entities with roles and permissions. 
   "account_id" is used for username part of email for new service account <account_id>@<project_id>.iam.gserviceaccount.com.
-  If "special_sa" it's true you can add role or permission for Google-managed service accounts.
-  If "role" not empty the set of predefined GCP roles will be assigned to this service account.
-  If "permissions" not empty a custom role with the set of permissions will be created.
-  If "generate_key" it's true the json key for service account will be created.
+  If "special_sa" is true you can add role or permission for Google-managed service accounts.
+  If "roles" not empty the set of predefined GCP roles will be assigned to this service account.
+  If "generate_key" is true the json key for service account will be created.
   EOT
   type = object({
     account_id   = string
     display_name = optional(string)
-    special_sa   = optional(bool)
-    role         = optional(set(string))
-    permissions  = optional(set(string))
-    generate_key = optional(bool)
+    special_sa   = optional(bool, false)
+    roles        = optional(set(string))
+    generate_key = optional(bool, false)
   })
-  default = {
-    account_id = ""
-  }
+  default = null
+}
+
+variable "custom_role" {
+  description = "IAM custom role and permissions"
+  type = object({
+    name        = string
+    permissions = set(string)
+  })
+  default = null
 }
